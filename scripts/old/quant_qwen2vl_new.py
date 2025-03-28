349a12ae6c983f0904168314b6f3be46e18e63fe
Renjie Wu <w33214321@126.com>
2025-03-28 11:29:57 +0800

    init

---
+++ scripts/old/quant_qwen2vl_new.py
@@ -0,0 +1,121 @@
+import json
+
+from qwen_vl_utils import process_vision_info
+from transformers import Qwen2VLProcessor
+
+
+from gptqmodel import GPTQModel, QuantizeConfig
+
+
+
+model_path = "/data/models/qwen2vl_72b_all_P1P2_28122024_20eps_merged"
+quant_path = "/data/models/qwen2vl_72b_all_P1P2_28122024_20eps_merged_py_script_4bit_test4"
+quantize_config = QuantizeConfig(
+    bits=4,  # 4 or 8
+    group_size=128,
+    damp_percent=0.1,
+    desc_act=False,  # set to False can significantly speed up inference but the perplexity may slightly bad
+    static_groups=False,
+    sym=True,
+    true_sequential=True,
+)
+
+# Load your processor and model with AutoGPTQ
+# device_map = {}
+# device_map['language_model.model.rotary_emb'] = 0
+processor = Qwen2VLProcessor.from_pretrained(model_path)
+# model = Qwen2VLGPTQForConditionalGeneration.from_pretrained(model_path, quantize_config)
+# max_memory = {0: "4GIB", 1: "50GIB", 2: "50GIB",'cpu': "300GIB"}
+# max_memory.update({i: f"{1}GIB" for i in range(8)})
+# max_memory.update({'cpu': "400GIB"})
+# max_memory[0] = "1GIB"
+model = GPTQModel.load(model_path, quantize_config,)
+
+# Then you need to prepare your data for calibaration. What you need to do is just put samples into a list,
+# each of which is a typical chat message as shown below. you can specify text and image in `content` field:
+# dataset = [
+#     # message 0
+#     [
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {"role": "user", "content": "Tell me who you are."},
+#         {"role": "assistant", "content": "I am a large language model named Qwen..."},
+#     ],
+#     # message 1
+#     [
+#         {
+#             "role": "user",
+#             "content": [
+#                 {"type": "image", "image": "file:///path/to/your/image.jpg"},
+#                 {"type": "text", "text": "Output all text in the image"},
+#             ],
+#         },
+#         {"role": "assistant", "content": "The text in the image is balabala..."},
+#     ],
+#     # other messages...
+#     ...,
+# ]
+# here, we use a caption dataset **only for demonstration**. You should replace it with your own sft dataset.
+def prepare_dataset(n_sample: int = 500) -> list[list[dict]]:
+    dataset_path = '/data/dataset/qwen2vl_out800_entity20_pos0.9_neg0.2_with_text_9178_fixed.json'
+    with open(dataset_path) as f:
+        dataset = json.load(f)[:n_sample]
+    return [
+        [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "image", "image": sample['images'][0]},
+                    {"type": "text", "text": sample['messages'][0]['content'].replace('<image>', '')},
+                ],
+            },
+            {"role": "assistant", "content": sample['messages'][1]['content']},
+        ]
+        for sample in dataset
+    ]
+
+
+dataset = prepare_dataset(n_sample=10)
+
+
+def batched(iterable, n: int):
+    # batched('ABCDEFG', 3) → ABC DEF G
+    assert n >= 1, "batch size must be at least one"
+    from itertools import islice
+
+    iterator = iter(iterable)
+    while batch := tuple(islice(iterator, n)):
+        yield batch
+
+
+calib_data = []
+batch_size = 1
+text = None
+for batch in batched(dataset, batch_size):
+    text = processor.apply_chat_template(batch, tokenize=False, add_generation_prompt=True)
+    image_inputs, video_inputs = process_vision_info(batch)
+    inputs = processor(text=text, images=image_inputs, videos=video_inputs, padding=True, return_tensors="pt")
+    calib_data.append(inputs)
+print(f'text: {text}')
+print(f'dataset length: {len(calib_data)}')
+print(f'\n\n------------data preparing has finished------------------\n\n')
+
+
+# Then just run the calibration process by one line of code:
+#model.to(device_map)
+model.quantize(dataset, batch_size=2, calibration_enable_gpu_cache=False)
+
+# Finally, save the quantized model:
+model.save(quant_path)
+# processor.save_pretrained(quant_path)
+
+# Optional: save the quantized model compatible with transformers
+# from optimum.gptq import GPTQQuantizer
+# from transformers.utils.quantization_config import QuantizationMethod
+# gptq_quantizer = GPTQQuantizer.from_dict(model.quantize_config.to_dict())
+# model.model.is_quantized = True
+# model.model.quantization_method = QuantizationMethod.GPTQ
+# model.model.config.quantization_config = gptq_quantizer.to_dict()
+# gptq_quantizer.save(model.model, quant_path, max_shard_size="4GB", safe_serialization=True)
+# processor.save_pretrained(quant_path)
+
+# Then you can obtain your own GPTQ quantized model for deployment. Enjoy!
